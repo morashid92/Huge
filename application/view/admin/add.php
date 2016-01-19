2d89e9c0090ce0051fb42ec41e0d795783729ee6
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-01-19 23:23:07 +0000

    delete ticket_type

--- application/view/admin/add.php
+++ application/view/admin/add.php
@@ -10,7 +10,7 @@ $checkbox1 = $_POST['chk1'];
 if($_POST['Submit']=="Submit")
 {
 	for ($i=0; $i<sizeof($checkbox1);$i++) {
-		$query="INSERT INTO `train` (train_no, train_name, source, destination, distance, day, month, year, departs, arrives) VALUES ('$_POST[trainno]', '$_POST[trainname]', '$_POST[source]', '$_POST[destination]','$_POST[distance]','$_POST[day]','$_POST[month]','$_POST[year]','$_POST[depart]','$_POST[arrive]', '".$checkbox1[$i]."')";
+		$query="INSERT INTO `train` (train_no, train_name, source, destination, distance, day, month, year, departs, arrives) VALUES ('$_POST[trainno]', '$_POST[trainname]', '$_POST[source]', '$_POST[destination]','$_POST[distance]','$_POST[day]','$_POST[month]','$_POST[year]','$_POST[depart]','$_POST[arrive]')";
 		mysql_query($query) or die(mysql_error());
 	}
 	echo "Record is inserted";
